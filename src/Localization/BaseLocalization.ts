e284910ba9d03429b1e0c8aceb1c11d0ad3b597f
Amol Kulkarni <amol.a.kulkarni@capgemini.com>
2022-05-31 09:05:27 +0530

    localization code changes
    code clean up

---
+++ src/Localization/BaseLocalization.ts
@@ -0,0 +1,27 @@
+import { LocalizationManager } from './LocalizationManager';
+
+/**
+ * Add string here
+ * also add translation in Locales file
+ */
+export const BaseLocalization = {
+    welcome: LocalizationManager.getLocalizedStrings('welcome'),
+    howAreYou: LocalizationManager.getLocalizedStrings('howAreYou'),
+    noInternetConnection: LocalizationManager.getLocalizedStrings('noInternetConnection'),
+    favoriteTitle: LocalizationManager.getLocalizedStrings('favoriteTitle'),
+    favoriteSubTitle: LocalizationManager.getLocalizedStrings('favoriteSubTitle'),
+    settingTitle: LocalizationManager.getLocalizedStrings('settingTitle'),
+    settingSubTitle: LocalizationManager.getLocalizedStrings('settingSubTitle'),
+    generalTitle: LocalizationManager.getLocalizedStrings('generalTitle'),
+    technicalTitle: LocalizationManager.getLocalizedStrings('technicalTitle'),
+    contact: LocalizationManager.getLocalizedStrings('contact'),
+    countryTitle: LocalizationManager.getLocalizedStrings('countryTitle'),
+    contentTitle: LocalizationManager.getLocalizedStrings('contentTitle'),
+    updateTitle: LocalizationManager.getLocalizedStrings('updateTitle'),
+    selectCategory: LocalizationManager.getLocalizedStrings('selectCategory'),
+    appInformation: LocalizationManager.getLocalizedStrings('appInformation'),
+    version: LocalizationManager.getLocalizedStrings('version'),
+    modificationDate: LocalizationManager.getLocalizedStrings('modificationDate'),
+    contentUpdates: LocalizationManager.getLocalizedStrings('contentUpdates'),
+    moreInfoTitle: LocalizationManager.getLocalizedStrings('moreInformation'),
+};
